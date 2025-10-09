90db23eb31d9476ac56a9ebaa24baf7b9b8adebd
Far1ck <salyakhov.farit@gmail.com>
2025-10-09 17:04:08 +0500

    Initial Commit

---
+++ src/main/java/com/star/recommendationservice/repository/RecommendationsRepository.java
@@ -0,0 +1,16 @@
+package com.star.recommendationservice.repository;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RecommendationsRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public RecommendationsRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Здесь методы с запросами...
+}
